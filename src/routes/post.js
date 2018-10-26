1efa02c2dca63147e7d99c1941a98aa7ac7c27d7
좌 준협 <jwajunhyeob@jwaui-MacBook-Air.local>
2018-10-26 20:05:35 +0900

    Done

--- src/routes/post.js
+++ src/routes/post.js
@@ -1,7 +1,12 @@
 import express from "express";
 import request from "request";
+import cheerio from "cheerio";
 
-const url = "http://www.mcdonalds.co.kr/www/kor/menu/menu_list.do?cate_cd=100";
+const mcdonaldsURL =
+  "https://www.mcdonalds.co.kr/www/kor/menu/menu_list.do?cate_cd=100";
+const naverURL = "https://www.naver.com/";
+
+const url = naverURL;
 
 const router = express.Router();
 
@@ -11,12 +16,36 @@ router.get("/", (req, res) => {
   request(url, (error, response, body) => {
     if (error) throw error;
 
-    console.log(body);
+    let resultArr = [];
+
+    const $ = cheerio.load(body);
+
+    let rank = "";
+    let keyword = "";
+    let realTime = {};
+
+    // console.log($(".ah_item")[0].children[1].children[1].children[0].data);
+    // console.log($(".ah_item")[0].children[1].children[3].children[0].data);
+    let colArr = $(".ah_item");
+    for (let i = 0; i < colArr.length / 2; i++) {
+      rank = colArr[i].children[1].children[1].children[0].data;
+      keyword = colArr[i].children[1].children[3].children[0].data;
+      realTime = {
+        rank,
+        keyword
+      };
+      console.log(colArr[i].children[1].children[1].children[0].data);
+      console.log(colArr[i].children[1].children[3].children[0].data);
+      resultArr.push(realTime);
+    }
+
     return res.json({
-      body
+      resultArr
     });
   });
 });
+
+//밑에는 무시
 
 router.post("/", (req, res) => {
   console.log(req.body.contents);
